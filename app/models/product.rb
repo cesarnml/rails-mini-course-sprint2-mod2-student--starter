b249a1491eb1a2ea8852de1bbba7974da152c189
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-08-20 17:22:11 -0700

    completes step 3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  def available?
+    puts inventory
+    inventory > 0
+  end
+
+  def reduce_inventory
+    new_inventory = inventory - 1
+    update(inventory: new_inventory)
+  end
 end
